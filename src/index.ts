ca0d689bb09d3d41b7e7278b1c251f9da014fead
Arjan van Wijk <thanarie@gmail.com>
2022-12-02 13:04:59 +0100

    Fix import path to useUnmount hook

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 /* PLOP_ADD_EXPORT */
 export * from './useIsomorphicLayoutEffect/useIsomorphicLayoutEffect';
 export * from './useMount/useMount';
-export * from './useMount/useUnmount';
+export * from './useUnmount/useUnmount';
 export * from './useRegisterRef/useRegisterRef';
 export * from './useToggle/useToggle';
