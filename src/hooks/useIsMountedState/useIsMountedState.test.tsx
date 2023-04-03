8c335329c20ce24291c074bc510fca0f18f5c6b3
Arjan van Wijk <thanarie@gmail.com>
2023-04-03 19:22:43 +0200

    Make useIsMountedState async

--- src/hooks/useIsMountedState/useIsMountedState.test.tsx
+++ src/hooks/useIsMountedState/useIsMountedState.test.tsx
@@ -1,26 +1,31 @@
 import { jest } from '@jest/globals';
-import { renderHook } from '@testing-library/react';
+import { act, renderHook } from '@testing-library/react';
 import { useIsMountedState } from './useIsMountedState.js';
 
 describe('useIsMountedState', () => {
   it('should not crash', async () => {
-    renderHook(() => useIsMountedState());
+    await act(async () => {
+      renderHook(() => useIsMountedState());
+    });
   });
 
   it('should update after rendering', async () => {
     const spy = jest.fn();
+
     const {
       result: { current: isMounted },
-    } = renderHook(() => {
-      // eslint-disable-next-line @typescript-eslint/no-shadow
-      const isMounted = useIsMountedState();
+    } = await act(async () =>
+      renderHook(() => {
+        // eslint-disable-next-line @typescript-eslint/no-shadow
+        const isMounted = useIsMountedState();
 
-      // this hook automatically re-renders because the `useIsMountedState` hook uses `useEffect`
-      // to update its state, so we need to capture the value _before_ the useEffect is executed.
-      spy(isMounted);
+        // this hook automatically re-renders because the `useIsMountedState` hook uses `useEffect`
+        // to update its state, so we need to capture the value _before_ the useEffect is executed.
+        spy(isMounted);
 
-      return isMounted;
-    });
+        return isMounted;
+      }),
+    );
 
     expect(isMounted).toBe(true);
     expect(spy).toBeCalledTimes(2);
