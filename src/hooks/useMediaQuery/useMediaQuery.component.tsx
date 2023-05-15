d6b3f93d70d23c84de377dadae7fc41e8f804854
Leroy Korterink <leroy@mediamonks.com>
2023-05-15 18:15:58 +0200

    Allow custom media query string

--- src/hooks/useMediaQuery/useMediaQuery.component.tsx
+++ src/hooks/useMediaQuery/useMediaQuery.component.tsx
@@ -2,8 +2,8 @@ import type { ReactElement } from 'react';
 import { useMediaQuery } from './useMediaQuery.js';
 
 export function UseMediaQueryComponent(): ReactElement {
-  // @ts-expect-error variable is defined in index.html
-  const isMinWidth480px = useMediaQuery('--min-width-480');
+  const isMinWidth480pxUsingVariable = useMediaQuery('--min-width-480');
+  const isMinWidth480pxUsingQuery = useMediaQuery('(min-width: 480px)');
 
-  return <div>{isMinWidth480px ? 'large' : 'small'}</div>;
+  return <div>{isMinWidth480pxUsingVariable && isMinWidth480pxUsingQuery ? 'large' : 'small'}</div>;
 }
--- src/hooks/useMediaQuery/useMediaQuery.ts
+++ src/hooks/useMediaQuery/useMediaQuery.ts
@@ -2,52 +2,70 @@ import { useEffect, useState } from 'react';
 import { useEventListener } from '../useEventListener/useEventListener.js';
 
 /**
- * The MediaQuery type is used for the `mediaQueryVariableName` parameter of
+ * The MediaQueryVariables type is used for the `mediaQueryOrVariableName` parameter of
  * `useMediaQuery`.
  *
- * Augment the MediaQuery interface to add the names for the CSS variables that
+ * Augment the MediaQueryVariables interface to add the names for the CSS variables that
  * describe media queries in your project.
  *
  * @example
+ * import '@mediamonks/react-hooks';
+ *
  * declare module '@mediamonks/react-hooks' {
- *   interface MediaQuery {
- *     '--media-query-name': string;
+ *   interface MediaQueryVariables {
+ *     '--media-query-name': never;
  *   }
  * }
  */
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface MediaQuery {}
+export interface MediaQueryVariables {}
 
-type MediaQueries = keyof MediaQuery;
+/**
+ * Enables auto-completion for the variable names that are defined in `MediaQueryVariables` and also allows custom string values.
+ */
+type MediaQueryValues =
+  // eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents
+  | keyof MediaQueryVariables
+  | (string & {
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      __custom_query__do_not_use_this_field__?: boolean;
+    });
 
 export function getMediaQueryList(
-  mediaQueryVariableName: MediaQueries,
+  mediaQueryOrVariableName: MediaQueryValues,
 ): MediaQueryList | undefined {
   if (typeof matchMedia === 'undefined') {
     return undefined;
   }
 
-  return matchMedia(getComputedStyle(document.body).getPropertyValue(mediaQueryVariableName));
+  const mediaQuery = mediaQueryOrVariableName.startsWith('--')
+    ? getComputedStyle(document.body).getPropertyValue(mediaQueryOrVariableName)
+    : mediaQueryOrVariableName;
+
+  return matchMedia(mediaQuery);
 }
 
 /**
  * Hook that returns a boolean indicating whether the media query matches.
  *
- * @param mediaQueryVariableName - The name of the CSS variable that describes the media query.
+ * @param mediaQueryOrVariableName - The name of the CSS variable that describes the media query.
  * @param defaultValue - The default value to return if the matchMedia API is not available.
  */
-export function useMediaQuery(mediaQueryVariableName: MediaQueries, defaultValue = false): boolean {
+export function useMediaQuery(
+  mediaQueryOrVariableName: MediaQueryValues,
+  defaultValue = false,
+): boolean {
   const [mediaQueryList, setMediaQueryList] = useState<MediaQueryList | undefined>(() =>
-    getMediaQueryList(mediaQueryVariableName),
+    getMediaQueryList(mediaQueryOrVariableName),
   );
   const [matches, setMatches] = useState<boolean | undefined>(defaultValue);
 
   useEffect(() => {
-    const newMediaQueryList = getMediaQueryList(mediaQueryVariableName);
+    const newMediaQueryList = getMediaQueryList(mediaQueryOrVariableName);
 
     setMediaQueryList(newMediaQueryList);
     setMatches(newMediaQueryList?.matches);
-  }, [defaultValue, mediaQueryList?.matches, mediaQueryVariableName]);
+  }, [defaultValue, mediaQueryList?.matches, mediaQueryOrVariableName]);
 
   useEventListener(mediaQueryList, 'change', (event) => {
     if (event instanceof MediaQueryListEvent) {
