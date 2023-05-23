ae60c451f040e0ad927da8a88e8561a61ca42dad
Leroy Korterink <leroy@mediamonks.com>
2023-05-23 14:32:30 +0200

    Add AutoFill preview to docs #61

--- src/components/AutoFill/AutoFill.stories.tsx
+++ src/components/AutoFill/AutoFill.stories.tsx
@@ -1,82 +1,98 @@
 /* eslint-disable react/jsx-no-literals, react/no-multi-comp */
+import { type Meta, type StoryObj } from '@storybook/react';
 import type { ReactElement } from 'react';
 import { AutoFill } from './AutoFill.js';
 
-export default {
+const meta = {
   title: 'components/AutoFill',
-};
+  component: AutoFill,
+} satisfies Meta;
 
-export function Horizontal(): ReactElement {
-  return (
-    <div
-      style={{
-        width: 520,
-        display: 'flex',
-        outline: '1px solid blue',
-      }}
-    >
-      <AutoFill axis="x">
-        <div
-          style={{
-            padding: 15,
-            flex: '0 1 auto',
-          }}
-        >
+type Story = StoryObj<typeof meta>;
+
+export default meta;
+
+export const Horizontal = {
+  render(): ReactElement {
+    return (
+      <div
+        style={{
+          width: 520,
+          display: 'flex',
+          outline: '1px solid blue',
+        }}
+      >
+        <AutoFill axis="x">
           <div
             style={{
-              width: 50,
-              height: 50,
-              outline: '1px solid red',
+              padding: 15,
+              flex: '0 1 auto',
             }}
-          />
-        </div>
+          >
+            <div
+              style={{
+                width: 50,
+                height: 50,
+                outline: '1px solid red',
+              }}
+            />
+          </div>
 
-        <div
-          style={{
-            padding: 15,
-            flex: '0 1 auto',
-          }}
-        >
           <div
             style={{
-              width: 75,
-              height: 50,
-              outline: '1px solid yellow',
+              padding: 15,
+              flex: '0 1 auto',
             }}
-          />
-        </div>
-      </AutoFill>
-    </div>
-  );
-}
+          >
+            <div
+              style={{
+                width: 75,
+                height: 50,
+                outline: '1px solid yellow',
+              }}
+            />
+          </div>
+        </AutoFill>
+      </div>
+    );
+  },
+  args: {
+    children: <div />,
+  },
+} satisfies Story;
 
-export function Vertical(): ReactElement {
-  return (
-    <div
-      style={{
-        height: 520,
-        width: 80,
-        display: 'flex',
-        flexDirection: 'column',
-        outline: '1px solid blue',
-      }}
-    >
-      <AutoFill axis="y">
-        <div
-          style={{
-            padding: 15,
-            flex: '0 1 auto',
-          }}
-        >
+export const Vertical = {
+  render(): ReactElement {
+    return (
+      <div
+        style={{
+          height: 520,
+          width: 80,
+          display: 'flex',
+          flexDirection: 'column',
+          outline: '1px solid blue',
+        }}
+      >
+        <AutoFill axis="y">
           <div
             style={{
-              width: 50,
-              height: 50,
-              outline: '1px solid red',
+              padding: 15,
+              flex: '0 1 auto',
             }}
-          />
-        </div>
-      </AutoFill>
-    </div>
-  );
-}
+          >
+            <div
+              style={{
+                width: 50,
+                height: 50,
+                outline: '1px solid red',
+              }}
+            />
+          </div>
+        </AutoFill>
+      </div>
+    );
+  },
+  args: {
+    children: <div />,
+  },
+} satisfies Story;
