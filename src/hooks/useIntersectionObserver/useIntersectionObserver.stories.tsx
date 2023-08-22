be7eb337fa24de8057aaff22c0d0c8d6f2757180
Peter van der Noord <petervdn@mediamonks.com>
2023-08-22 11:55:16 +0200

    Add story for useIntersectionObserver

--- src/hooks/useIntersectionObserver/useIntersectionObserver.stories.tsx
+++ src/hooks/useIntersectionObserver/useIntersectionObserver.stories.tsx
@@ -1,28 +1,31 @@
 /* eslint-disable react/jsx-no-bind, react/no-multi-comp, react/jsx-no-literals */
 import type { StoryObj } from '@storybook/react';
 import type { ReactElement } from 'react';
+import { useRef, useState } from 'react';
+import { useIntersectionObserver } from './useIntersectionObserver.js';
 
 export default {
   title: 'hooks/useIntersectionObserver',
 };
 
 function DemoComponent(): ReactElement {
+  const [isRedVisible, setIsRedVisible] = useState(false);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const targetRef = useRef<HTMLDivElement>(null);
+  useIntersectionObserver(targetRef, ([entry]) => {
+    setIsRedVisible(entry.isIntersecting);
+  });
+
   return (
-    <div>
-      <div className="alert alert-primary">
-        <h4 className="alert-heading">Instructions!</h4>
-        <p className="mb-0">Add instructions about the hook here.</p>
+    <>
+      <p>
+        Scroll down in green element, red is currently{' '}
+        <strong>{isRedVisible ? '' : 'NOT'} VISIBLE</strong>
+      </p>
+      <div ref={containerRef} style={{ height: 100, overflow: 'scroll', backgroundColor: 'green' }}>
+        <div ref={targetRef} style={{ height: 100, marginTop: 200, backgroundColor: 'red' }}></div>
       </div>
-      <div>
-        Value: <span className="badge rounded-pill bg-primary">3</span>
-      </div>
-      <div className="card border-dark" data-ref="test-area">
-        <div className="card-header">Test Area</div>
-        <div className="card-body">
-          <p>TODO: Implement a test case for the hook.</p>
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
 
