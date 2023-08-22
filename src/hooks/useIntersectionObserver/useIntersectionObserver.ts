a3465f985054110fe14ff7bee97f22399d8d2181
Peter van der Noord <petervdn@mediamonks.com>
2023-08-22 12:46:50 +0200

    Allow multiple targets for useIntersectionObserver

--- src/hooks/useIntersectionObserver/useIntersectionObserver.ts
+++ src/hooks/useIntersectionObserver/useIntersectionObserver.ts
@@ -8,28 +8,30 @@ import { unref, type Unreffable } from '../../utils/unref/unref.js';
  * For a full explanation of the IntersectionObserver API, check the docs:
  * https://developer.mozilla.org/en-US/docs/Web/API/IntersectionObserver/IntersectionObserver
  *
- * @param target - The target to observe
+ * @param targetOrTargets - The target or targets to observe
  * @param callback - The callback to fire when the element resizes
  * @param options - Optional options object
  */
 
 export function useIntersectionObserver(
-  target: Unreffable<Element | null>,
+  targetOrTargets: Unreffable<Element | null> | Array<Unreffable<Element | null>>,
   callback: IntersectionObserverCallback,
   options?: IntersectionObserverInit,
 ): void {
   useEffect(() => {
-    const element = unref(target);
-
-    if (element === null) {
-      return;
-    }
+    const elements = (Array.isArray(targetOrTargets) ? targetOrTargets : [targetOrTargets]).map(
+      (target) => unref(target),
+    );
 
     const intersectionObserverInstance = new IntersectionObserver(callback, options);
-    intersectionObserverInstance.observe(element);
+    for (const element of elements) {
+      if (element) {
+        intersectionObserverInstance.observe(element);
+      }
+    }
 
     return () => {
       intersectionObserverInstance.disconnect();
     };
-  }, [callback, options, target]);
+  }, [callback, options, targetOrTargets]);
 }
