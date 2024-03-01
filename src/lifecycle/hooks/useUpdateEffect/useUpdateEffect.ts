2ee2da2df680cffe405ee3028d0dccaea9ca2e81
Lukas <lukas.feitsma@mediamonks.com>
2024-03-01 14:15:07 +0100

    Move to lifecycle folder

--- src/lifecycle/hooks/useUpdateEffect/useUpdateEffect.ts
+++ src/lifecycle/hooks/useUpdateEffect/useUpdateEffect.ts
@@ -1,15 +1,13 @@
-import { noop } from 'lodash-es';
 import { type DependencyList, type EffectCallback, useEffect } from 'react';
 import { useIsFirstRender } from '../useIsFirstRender/useIsFirstRender.js';
 
 /**
- * This hook ignores the first render aka not invoked on mount.
+ * This hook ignores the first render, so it's not invoked on mount.
  *
  * @param effect Function to run on updates
  * @param deps Dependencies list, as for `useEffect` hook
  */
 export function useUpdateEffect(effect: EffectCallback, deps?: DependencyList): void {
-  const isFirstMount = useIsFirstRender();
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect(isFirstMount ? noop : effect, deps);
+  useEffect(useIsFirstRender() ? (): undefined => undefined : effect, deps);
 }
